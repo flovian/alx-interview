d0f5efdc4418a7541fb249c05a233cbfbed83796
flovian <45689069+flovian@users.noreply.github.com>
2023-01-20 22:46:28 +0300

    Correction

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,23 @@
 #!/usr/bin/python3
-"""
-Parses Logs
-"""
+'''A script that generates random HTTP request logs.
+'''
+import random
 import sys
+import datetime
+from time import sleep
 
-if __name__ == "__main__":
-    num = 0
-    FileSize = 0
-    stts = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0,
-    }
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    try:
-        for line in sys.stdin:
-            num += 1
-            read_line = line.split(" ")
-            if len(read_line) > 2:
-                FileSize += int(read_line[-1])
-                if read_line[-2] in stts:
-                    stts[read_line[-2]] += 1
-            if num % 10 == 0:
-                print("File size: {}".format(FileSize))
-                for code in codes:
-                    if stts[code]:
-                        print("{}: {}".format(code, stts[code]))
-    except KeyboardInterrupt:
-        pass
-    finally:
-        print("File size: {}".format(FileSize))
-        for code in codes:
-            if stts[code]:
-                print("{}: {}".format(code, stts[code]))
+
+for i in range(10000):
+    sleep(random.random())
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET {} {}\" {} {}\n".format(
+        random.randint(1, 255),
+        random.randint(1, 255),
+        random.randint(1, 255),
+        random.randint(1, 255),
+        datetime.datetime.now(),
+        '/projects/1216',
+        'HTTP/1.1',
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    sys.stdout.flush()
