861d8e297065ad4f8e88446198ea803f6af4f216
flovian <45689069+flovian@users.noreply.github.com>
2023-01-20 22:44:12 +0300

    Fix code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,46 +1,40 @@
 #!/usr/bin/python3
-
-""" script that reads stdin line by line and computes metrics """
-
+"""
+Parses Logs
+"""
 import sys
 
-
-def printStatus(dic, size):
-    """ Prints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
-
-
-# sourcery skip: use-contextlib-suppress
-statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-               "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
-
-try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printStatus(statusCodes, size)
-
-        stlist = line.split()
-        count += 1
-
-        try:
-            size += int(stlist[-1])
-        except Exception:
-            pass
-
-        try:
-            if stlist[-2] in statusCodes:
-                statusCodes[stlist[-2]] += 1
-        except Exception:
-            pass
-    printStatus(statusCodes, size)
-
-
-except KeyboardInterrupt:
-    printStatus(statusCodes, size)
-    raise
+if __name__ == "__main__":
+    num = 0
+    FileSize = 0
+    stts = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    try:
+        for line in sys.stdin:
+            num += 1
+            read_line = line.split(" ")
+            if len(read_line) > 2:
+                FileSize += int(read_line[-1])
+                if read_line[-2] in stts:
+                    stts[read_line[-2]] += 1
+            if num % 10 == 0:
+                print("File size: {}".format(FileSize))
+                for code in codes:
+                    if stts[code]:
+                        print("{}: {}".format(code, stts[code]))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("File size: {}".format(FileSize))
+        for code in codes:
+            if stts[code]:
+                print("{}: {}".format(code, stts[code]))
